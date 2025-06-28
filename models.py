b83b6b96b9bd097e9aa8a086ec8156933e9b5cb9
Mohammedpasha786 <mohammedafreed827@gmail.com>
2025-06-28 23:45:45 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    category_id = Column(Integer, primary_key=True)
+    category_name = Column(String, nullable=False)
+
+    products = relationship('Product', back_populates='category')
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.category_id'))
+
+    category = relationship('Category', back_populates='products')
